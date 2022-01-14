765295f281f8cfe46554e137aa2688ae6c119d59
Geries Handal <gghandal@gmail.com>
2022-01-14 21:16:27 +0100

    Adds web app icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,47 @@ function MyDocument() {
   return (
     <Html lang="en">
       <Head>
+        <title>YAI Hacker News</title>
+        <meta charSet="UTF-8" />
+        <meta name="application-name" content="YAH" />
         <meta
           name="description"
           content="Yet another implementation of Hacker News"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-chrome-512x512.png"
+        />
+
+        <meta name="apple-mobile-web-app-title" content="My Site" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
       </Head>
       <body>
         <Main />
