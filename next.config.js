0c38739f44f531ddeca71cb888ec4077a52230f6
Geries Handal <gghandal@gmail.com>
2022-02-16 20:07:28 +0100

    updates packages and adds panelbear

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/teddy.js",
+        destination: "https://cdn.panelbear.com/analytics.js",
+      },
+    ];
+  },
   reactStrictMode: true,
   poweredByHeader: false,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import { usePanelbear } from '@panelbear/panelbear-nextjs';
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'lib/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Load Panelbear only once during the app lifecycle
+  usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_ID || '', {
+    scriptSrc: '/teddy.js',
+    honorDNT: true,
+  });
   return (
     <>
       <Head>
