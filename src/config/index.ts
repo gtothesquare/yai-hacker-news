e75e578d42c465dbde0e871ee056de9b13779a68
Geries Handal <gghandal@gmail.com>
2022-03-07 16:14:52 +0100

    Updates packages and adds faster comments api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export const HACKER_NEWS_API = process.env.HACKER_NEWS_API;
+export const HACKER_NEWS_ITEM_DETAILS_API = process.env.HACKER_NEWS_ITEM_DETAILS_API;
 export const REDIS_URL = process.env.REDIS_URL;
