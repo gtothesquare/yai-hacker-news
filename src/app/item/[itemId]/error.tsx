f80895bdcb59b8ae386b389ab933e1db9521a3a4
Geries Handal <gghandal@gmail.com>
2023-07-26 21:21:27 +0200

    fix: add error handling and refactor colors

---
+++ src/app/item/[itemId]/error.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>{"Could not load all comments, Vercel functions can't handle big payloads"}</h2>
+    </div>
+  );
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from '../ui/Link';
 
 export function Header() {
   return (
-    <header className="mb-2 py-1 w-full bg-orange-900">
+    <header className="mb-2 py-1 w-full bg-stone-700">
       <div className="mx-auto w-full max-w-5xl px-4 py-2">
         <h1 className="text-3xl font-thin">
           <Link href={'/'}>YHK - yet another hacker news clone</Link>
