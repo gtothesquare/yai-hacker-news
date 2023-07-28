af2cb9414b7b265b28cd4d97c543682d35905ed1
Geries Handal <gghandal@gmail.com>
2023-07-28 23:34:21 +0200

    fix: use Alert for error page

--- src/app/item/[itemId]/error.tsx
+++ src/app/item/[itemId]/error.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect } from 'react';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/Alert';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   useEffect(() => {
@@ -10,7 +12,14 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
 
   return (
     <div>
-      <h2>{"Could not load all comments, Vercel functions can't handle big payloads"}</h2>
+      <h2></h2>
+      <Alert variant="error">
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle>Error fetching comments</AlertTitle>
+        <AlertDescription>
+          {"Could not load all comments, Vercel functions can't handle big payloads"}
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
