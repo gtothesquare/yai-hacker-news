f23d3a7ec3b93b5e52b79f51b0ff759a31b5a990
Geries Handal <gghandal@gmail.com>
2023-08-03 16:39:21 +0200

    fix: experimenting with revalidaiton

--- src/lib/api/fetchAlgoliaData.ts
+++ src/lib/api/fetchAlgoliaData.ts
@@ -2,7 +2,7 @@ import 'server-only';
 import { HACKER_NEWS_ALGOLIA_API } from '@/config';
 
 export const fetchAlgoliaData = async <T>(path: string): Promise<T> => {
-  const res = await fetch(`${HACKER_NEWS_ALGOLIA_API}${path}`, { next: { revalidate: 0 } });
+  const res = await fetch(`${HACKER_NEWS_ALGOLIA_API}${path}`, { next: { revalidate: 10 } });
 
   if (res.status !== 200) {
     throw new Error(`Status ${res.status}`);
--- src/lib/api/fetchData.ts
+++ src/lib/api/fetchData.ts
@@ -2,7 +2,7 @@ import 'server-only';
 import { HACKER_NEWS_API } from '@/config';
 
 export const fetchData = async <T>(path: string): Promise<T> => {
-  const res = await fetch(`${HACKER_NEWS_API}/${path}.json`, { next: { revalidate: 0 } });
+  const res = await fetch(`${HACKER_NEWS_API}/${path}.json`, { next: { revalidate: 2 } });
 
   if (res.status !== 200) {
     throw new Error(`Status ${res.status}`);
